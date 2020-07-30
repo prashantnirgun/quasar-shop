326dfff1685acec3b12b24ecfc17e05bafd7a6e8
Prashant <prashant.nirgun@gmail.com>
2020-07-30 13:24:18 +0530

    brand sub-footer padding set

--- src/components/home/Sub-Footer.vue
+++ src/components/home/Sub-Footer.vue
@@ -1,23 +1,12 @@
 <template>
-  <div class="row">
+  <div class=" row wrap justify-evenly ">
     <div
-      id="parent"
-      class="row wrap justify-evenly  "
-      style="overflow: hidden;"
+      class="column col-2 col-xs-6 col-sm-4 col-md-2 col-lg-2 col-xl-2 q-pa-md items-center content-center"
+      style="overflow: auto;"
+      v-for="no in [1, 2, 3, 4, 5, 6, 7, 8]"
+      :key="no"
     >
-      <div
-        class="column col-6col-lg-2 col-md- 2 q-ma-sm items-center content-center"
-        style="overflow: auto;"
-        v-for="no in [1, 2, 3, 4, 5, 6, 7, 8]"
-        :key="no"
-      >
-        <q-img
-          class="col "
-          src="/brands/tss.png"
-          height="200px"
-          width="200px"
-        />
-      </div>
+      <q-img class="" src="/brands/tss.png" height="100px" width="100px" />
     </div>
   </div>
 </template>
