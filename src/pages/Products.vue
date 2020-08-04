4e86fa2b31050f7187d11e3568772dd25ae42aef
Prashant <prashant.nirgun@gmail.com>
2020-08-04 18:15:02 +0530

    product horizontal view image-center, badge is added

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -52,7 +52,9 @@
               class="cursor-pointer"
               :class="[
                 horizontal ? 'col-4' : 'col-12',
-                { 'q-pt-md': !!horizontal }
+                'row',
+                'items-center',
+                'justify-center'
               ]"
             >
               <q-img
@@ -60,7 +62,16 @@
                 spinner-color="primary"
                 spinner-size="82px"
                 placeholder-src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWBAMAAADOL2zRAAAAG1BMVEXMzMyWlpaqqqq3t7fFxcW+vr6xsbGjo6OcnJyLKnDGAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABAElEQVRoge3SMW+DMBiE4YsxJqMJtHOTITPeOsLQnaodGImEUMZEkZhRUqn92f0MaTubtfeMh/QGHANEREREREREREREtIJJ0xbH299kp8l8FaGtLdTQ19HjofxZlJ0m1+eBKZcikd9PWtXC5DoDotRO04B9YOvFIXmXLy2jEbiqE6Df7DTleA5socLqvEFVxtJyrpZFWz/pHM2CVte0lS8g2eDe6prOyqPglhzROL+Xye4tmT4WvRcQ2/m81p+/rdguOi8Hc5L/8Qk4vhZzy08DduGt9eVQyP2qoTM1zi0/uf4hvBWf5c77e69Gf798y08L7j0RERERERERERH9P99ZpSVRivB/rgAAAABJRU5ErkJggg=="
-              />
+              >
+                <q-badge
+                  v-if="props.row.tag"
+                  class="transparent no-margin no-padding"
+                >
+                  <q-chip icon="star" color="yellow" text-color="black">{{
+                    props.row.tag
+                  }}</q-chip>
+                </q-badge>
+              </q-img>
             </div>
 
             <div :class="[horizontal ? 'col-8' : 'col-12']">
