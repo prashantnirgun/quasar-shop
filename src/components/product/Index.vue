927a07920d45390fc8a32a4c97e67c408c3453a3
Prashant <prashant.nirgun@gmail.com>
2020-08-19 18:08:13 +0530

    product_filter prototype was created

--- src/components/product/Index.vue
+++ src/components/product/Index.vue
@@ -1,91 +1,98 @@
 <template>
-  <q-table
-    :data="sortedLists"
-    hide-header
-    row-key="name"
-    grid
-    hide-bottom
-    :pagination="initialPagination"
-  >
-    <template v-slot:top>
-      <q-chip color="primary" text-color="white" size="md" class="q-py-md">
-        <q-avatar color="black" text-color="white" class="text-bold">
-          {{ totalProducts }}
-        </q-avatar>
-        {{ categoryName }}
-      </q-chip>
-      <q-chip
-        v-if="isDesktop"
-        size="md"
-        class="q-py-md"
-        removable
-        v-model="eclair"
-        color="teal"
-        text-color="white"
-        icon="bookmark"
-      >
-        Hot Deals
-      </q-chip>
+  <div>
+    <q-table
+      :data="sortedLists"
+      hide-header
+      row-key="name"
+      grid
+      hide-bottom
+      :pagination="initialPagination"
+    >
+      <template v-slot:top>
+        <q-chip color="primary" text-color="white" size="md" class="q-py-md">
+          <q-avatar color="black" text-color="white" class="text-bold">
+            {{ totalProducts }}
+          </q-avatar>
+          {{ categoryName }}
+        </q-chip>
+        <q-chip
+          v-if="isDesktop"
+          size="md"
+          class="q-py-md"
+          removable
+          v-model="eclair"
+          color="teal"
+          text-color="white"
+          icon="bookmark"
+        >
+          Hot Deals
+        </q-chip>
 
-      <q-space />
-      <q-btn-group rounded>
-        <q-btn
-          dense
-          rounded
-          color="primary"
-          icon="list"
-          @click="horizontal = true"
-        />
-
-        <q-btn
-          dense
-          rounded
-          color="primary"
-          icon="grid_on"
-          @click="horizontal = false"
-        />
-
-        <q-btn dense rounded color="primary" icon="filter_alt" />
-
-        <q-btn-dropdown dense auto-close rounded color="primary" icon="sort">
-          <q-list
+        <q-space />
+        <q-btn-group rounded>
+          <q-btn
             dense
-            bordered
-            padding
-            class="rounded-borders"
-            style="width: 200px"
-          >
-            <q-item>
-              <q-item-section>
-                <q-toggle
-                  dense
-                  left-label
-                  color="blue"
-                  label="Price Low to High"
-                  v-model="selection"
-                  val="sale_rate"
-                  checked-icon="check"
-                  unchecked-icon="clear"
-                  @input="sort('sale_rate')"
-                />
-              </q-item-section>
-            </q-item>
-            <q-item>
-              <q-item-section>
-                <q-toggle
-                  dense
-                  left-label
-                  color="blue"
-                  label="Price High to Low"
-                  v-model="selection"
-                  val="sale_rate desc"
-                  checked-icon="check"
-                  unchecked-icon="clear"
-                  @input="sort('sale_rate desc')"
-                />
-              </q-item-section>
-            </q-item>
-            <!-- <q-item>
+            rounded
+            color="primary"
+            icon="list"
+            @click="horizontal = true"
+          />
+
+          <q-btn
+            dense
+            rounded
+            color="primary"
+            icon="grid_on"
+            @click="horizontal = false"
+          />
+
+          <q-btn
+            dense
+            rounded
+            color="primary"
+            icon="filter_alt"
+            @click="modal = true"
+          />
+
+          <q-btn-dropdown dense auto-close rounded color="primary" icon="sort">
+            <q-list
+              dense
+              bordered
+              padding
+              class="rounded-borders"
+              style="width: 200px"
+            >
+              <q-item>
+                <q-item-section>
+                  <q-toggle
+                    dense
+                    left-label
+                    color="blue"
+                    label="Price Low to High"
+                    v-model="selection"
+                    val="sale_rate"
+                    checked-icon="check"
+                    unchecked-icon="clear"
+                    @input="sort('sale_rate')"
+                  />
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-toggle
+                    dense
+                    left-label
+                    color="blue"
+                    label="Price High to Low"
+                    v-model="selection"
+                    val="sale_rate desc"
+                    checked-icon="check"
+                    unchecked-icon="clear"
+                    @input="sort('sale_rate desc')"
+                  />
+                </q-item-section>
+              </q-item>
+              <!-- <q-item>
               <q-item-section>
                 <q-toggle
                   dense
@@ -100,52 +107,54 @@
                 />
               </q-item-section>
             </q-item> -->
-            <q-item>
-              <q-item-section>
-                <q-toggle
-                  dense
-                  left-label
-                  color="blue"
-                  label="Saving Low to High"
-                  v-model="selection"
-                  val="saving"
-                  checked-icon="check"
-                  unchecked-icon="clear"
-                  @input="sort('saving')"
-                />
-              </q-item-section>
-            </q-item>
-            <q-item>
-              <q-item-section>
-                <q-toggle
-                  dense
-                  left-label
-                  color="blue"
-                  label="Saving High to Low"
-                  v-model="selection"
-                  val="saving desc"
-                  checked-icon="check"
-                  unchecked-icon="clear"
-                  @input="sort('saving desc')"
-                />
-              </q-item-section>
-            </q-item>
-            <!-- <q-item>
+              <q-item>
+                <q-item-section>
+                  <q-toggle
+                    dense
+                    left-label
+                    color="blue"
+                    label="Saving Low to High"
+                    v-model="selection"
+                    val="saving"
+                    checked-icon="check"
+                    unchecked-icon="clear"
+                    @input="sort('saving')"
+                  />
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-toggle
+                    dense
+                    left-label
+                    color="blue"
+                    label="Saving High to Low"
+                    v-model="selection"
+                    val="saving desc"
+                    checked-icon="check"
+                    unchecked-icon="clear"
+                    @input="sort('saving desc')"
+                  />
+                </q-item-section>
+              </q-item>
+              <!-- <q-item>
               <q-btn dense color="warning" label="Reset" />
               <q-space />
               <q-btn dense color="secondary" label="Apply" />
             </q-item> -->
-          </q-list>
-        </q-btn-dropdown>
-      </q-btn-group>
-    </template>
-    <template v-slot:item="props">
-      <div :class="outer">
-        <!-- {{ props.row.product_name }} -->
-        <card :data="props.row" :horizontal="horizontal" />
-      </div>
-    </template>
-  </q-table>
+            </q-list>
+          </q-btn-dropdown>
+        </q-btn-group>
+      </template>
+      <template v-slot:item="props">
+        <div :class="outer">
+          <!-- {{ props.row.product_name }} -->
+          <card :data="props.row" :horizontal="horizontal" />
+        </div>
+      </template>
+    </q-table>
+    <product-filter :show="modal" @close="modal = false" />
+  </div>
 </template>
 
 <script>
@@ -155,11 +164,13 @@ import device_mixin from 'src/mixins/device_mixin';
 export default {
   mixins: [device_mixin],
   components: {
-    card: () => import('./Card')
+    card: () => import('./Card'),
+    'product-filter': () => import('./Product_filter')
   },
   data() {
     return {
       eclair: true,
+      modal: false,
       selection: ['saving desc'],
       //selection: 'Saving High to Low',
       sortByHighToLow: true,
---
+++ src/components/product/Product_filter.vue
@@ -0,0 +1,109 @@
+<template>
+  <q-dialog v-model="show" persistent position="top">
+    <q-card style="min-width: 30vw">
+      <q-card-section class="row items-center q-pb-none">
+        <div class="text-h6">Filter</div>
+        <q-space />
+        <q-btn icon="close" flat round dense @click="close" />
+      </q-card-section>
+
+      <q-card-section>
+        <q-item>
+          <q-item-section avatar>
+            <q-icon name="local_atm" />
+          </q-item-section>
+          <q-item-section>
+            <q-range
+              v-model="standard"
+              markers
+              step="250"
+              :min="250"
+              :max="2500"
+              label-always
+              :left-label-value="'Rs ' + standard.min + ' Rs'"
+              :right-label-value="'Rs ' + standard.max + ' Rs'"
+            />
+          </q-item-section>
+        </q-item>
+        <q-item>
+          <!-- <q-item-section> -->
+          <q-input dense outlined v-model="min" label="Minimum" />
+          <q-input dense outlined v-model="max" label="Maximum" />
+          <!-- </q-item-section> -->
+        </q-item>
+
+        <q-item>
+          <q-item-section>
+            discount
+
+            <div class="q-gutter-sm">
+              <q-checkbox v-model="teal" label="Rs 250 - Rs 500" color="teal" />
+              <q-checkbox
+                v-model="orange"
+                label="Rs 500 - Rs 750"
+                color="orange"
+              />
+              <q-checkbox v-model="red" label="Rs 750 - Rs 1000" color="red" />
+              <q-checkbox
+                v-model="cyan"
+                label="Rs 1000 - Rs 2500"
+                color="cyan"
+              />
+            </div>
+          </q-item-section>
+        </q-item>
+
+        <q-item>
+          <q-item-section>
+            Packing
+
+            <div class="q-gutter-sm">
+              <q-checkbox v-model="teal" label="1 KG" color="teal" />
+
+              <q-checkbox v-model="red" label="5 KG" color="red" />
+              <q-checkbox v-model="cyan" label="15 KG" color="cyan" />
+            </div>
+          </q-item-section>
+        </q-item>
+
+        <q-item>
+          <q-item-section>
+            Offer
+
+            <div class="q-gutter-sm">
+              <q-checkbox v-model="teal" label="Sale" color="teal" />
+              <q-checkbox v-model="red" label="New Arrival" color="red" />
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: ['show'],
+  data() {
+    return {
+      standard: {
+        min: 250,
+        max: 2500
+      },
+      min: 0,
+      max: 0,
+      teal: true,
+      orange: false,
+      red: false,
+      cyan: true
+    };
+  },
+
+  methods: {
+    close() {
+      this.$emit('close');
+    }
+  },
+  computed: {}
+};
+</script>
